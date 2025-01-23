d48149e2d1065de0818119658d2e4f80c941a9d5
Pavel Golovin <pavel.golovin09@yandex.ru>
2025-01-23 18:43:51 +0300

    counter of repeated words in input

--- cmd/main.go
+++ cmd/main.go
@@ -1,67 +1,31 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 8.
+//!+
+
+// Dup1 prints the text of each line that appears more than
+// once in the standard input, preceded by its count.
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"sort"
+	"os"
 )
 
-// простой поиск элемента
-func simpleSearch(sl []int, n int) (int, string) {
-	for i, e := range sl {
-		if e == n {
-			return i, fmt.Sprintf("Найден элемент %v под индексом %v при помощи простого поиска. \n\n", e, i)
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
-
-	return -1, fmt.Sprintf("Не найден элемент %v в исходном слайсе при помощи простого поиска. \n\n", n)
 }
 
-// бинарный поиск элемента в слайсе
-func binarSearch(sl []int, n int) (int, string) {
-	if len(sl) == 0 {
-		return -1, fmt.Sprintf("Не найден элемент %v в исходном слайсе при помощи бинарного поиска. \n\n", n)
-	}
-
-	midElement := len(sl) / 2
-
-	if sl[midElement] == n {
-		return midElement, fmt.Sprintf("Найден элемент %v под индексом %v при помощи бинарного поиска. \n\n", n, midElement)
-
-	} else if sl[midElement] > n {
-		lowSl := sl[:midElement]
-		return binarSearch(lowSl, n)
-
-	} else if sl[midElement] < n {
-		res, text := binarSearch(sl[midElement+1:], n)
-		return res + midElement + 1, text
-	}
-
-	return -1, fmt.Sprintf("Не найден элемент %v в исходном слайсе при помощи бинарного поиска. \n\n", n)
-}
-
-func main() {
-	sl := []int{4, 5, 9, 1, 3, 2, 44, 31, 51}
-	sort.Ints(sl) // отсортировали числа по возрастанию [1 2 3 4 5 9 31 44 51]
-
-	fmt.Println(sl)
-
-	var inputNum int
-	fmt.Println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-	fmt.Print("Введите число: ")
-	fmt.Scan(&inputNum)
-
-	res, text1 := simpleSearch(sl, inputNum)
-	res2, text2 := binarSearch(sl, inputNum)
-
-	/////////// Вывод в консоль \\\\\\\\\\\\
-	fmt.Println("\n\nSimple Search:\n")
-	fmt.Println("input num:", inputNum)
-	fmt.Print("result: ", res, "\ntext: ", text1)
-
-	fmt.Println("============================")
-
-	fmt.Println("\nBinary Search:\n")
-	fmt.Println("input num:", inputNum)
-	fmt.Println("result:", res2, "\ntext:", text2)
-	//////////////////////\\\\\\\\\\\\\\\\\\\\\\\\
-}
+//!-
