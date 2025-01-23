5bcb97c78d64a8d6d3bf33e1dc9b650b6cfac9a9
Pavel Golovin <pavel.golovin09@yandex.ru>
2025-01-23 23:11:51 +0300

    simple http get request

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,26 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 8.
-//!+
-
-// Dup1 prints the text of each line that appears more than
-// once in the standard input, preceded by its count.
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 func main() {
-	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-	// NOTE: ignoring potential errors from input.Err()
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("ошибка fetch: %v\n", err)
 		}
+
+		body, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Printf("ошибка fetch чтение: %v", err)
+			os.Exit(1)
+		}
+
+		fmt.Print(body)
 	}
 }
-
-//!-
